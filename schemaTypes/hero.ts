e57c8c6e80f30489d660b661ffb37837eb173992
S5HX <yarulinainur@gmail.com>
2026-04-29 17:22:33 +0700

    add hero schema for WWII heroes

---
+++ schemaTypes/hero.ts
@@ -0,0 +1,54 @@
+import {defineField, defineType} from 'sanity'
+
+export const hero = defineType({
+  name: 'hero',
+  title: 'Герой',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'name',
+      title: 'Имя',
+      type: 'string',
+      validation: (r) => r.required(),
+    }),
+    defineField({
+      name: 'photo',
+      title: 'Фото',
+      type: 'image',
+      options: {hotspot: true},
+    }),
+    defineField({
+      name: 'shortDescription',
+      title: 'Краткое описание',
+      type: 'text',
+      rows: 2,
+    }),
+    defineField({
+      name: 'description',
+      title: 'Полное описание',
+      type: 'array',
+      of: [{type: 'block'}],
+    }),
+    defineField({
+      name: 'birthYear',
+      title: 'Год рождения',
+      type: 'number',
+    }),
+    defineField({
+      name: 'deathYear',
+      title: 'Год смерти',
+      type: 'number',
+    }),
+    defineField({
+      name: 'rank',
+      title: 'Звание/должность',
+      type: 'string',
+    }),
+  ],
+  preview: {
+    select: {media: 'photo', title: 'name'},
+  },
+  orderings: [
+    {by: [{direction: 'asc', field: 'birthYear'}], name: 'birthYearAsc', title: 'Год рождения'},
+  ],
+})
--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,6 +1,7 @@
 import {announcement} from './announcement'
 import {exhibit} from './exhibit'
 import {exhibition} from './exhibition'
+import {hero} from './hero'
 import {schoolInfo} from './school-info'
 
-export const schemaTypes = [exhibition, exhibit, announcement, schoolInfo]
+export const schemaTypes = [exhibition, exhibit, announcement, schoolInfo, hero]
